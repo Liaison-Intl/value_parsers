ec0708f7f76036ed40a7374ea402c9adc00a7f82
Zac Stewart <zgstewart@gmail.com>
2013-08-28 10:41:32 -0400

    Test new boolean parser values

--- spec/value_parsers/boolean_parser_spec.rb
+++ spec/value_parsers/boolean_parser_spec.rb
@@ -29,6 +29,16 @@ describe BooleanParser do
     it { subject.humanize(true).must_equal 'Yes'}
   end
 
+  describe 'parsing "yes"' do
+    it { subject.parse('yes').must_equal true }
+    it { subject.humanize('yes').must_equal 'Yes'}
+  end
+
+  describe 'parsing "true"' do
+    it { subject.parse('true').must_equal true }
+    it { subject.humanize('true').must_equal 'Yes'}
+  end
+
   describe 'parsing 0' do
     it { subject.parse(0).must_equal false }
     it { subject.humanize(0).must_equal 'No'}
@@ -54,6 +64,16 @@ describe BooleanParser do
     it { subject.humanize("N").must_equal 'No'}
   end
 
+  describe 'parsing "no"' do
+    it { subject.parse("no").must_equal false }
+    it { subject.humanize("no").must_equal 'No'}
+  end
+
+  describe 'parsing "false"' do
+    it { subject.parse("false").must_equal false }
+    it { subject.humanize("false").must_equal 'No'}
+  end
+
   describe 'parsing false' do
     it { subject.parse(false).must_equal false }
     it { subject.humanize(false).must_equal 'No'}
@@ -77,6 +97,11 @@ describe BooleanParser do
   describe 'parsing "NONE"' do
     it { subject.parse('NONE').must_equal nil }
     it { subject.humanize('NONE').must_equal 'Not Sure' }
+  end
+
+  describe 'parsing the empty string' do
+    it { subject.parse('').must_equal nil }
+    it { subject.humanize('').must_equal 'Not Sure' }
   end
 
   describe 'parsing "garbage"' do
