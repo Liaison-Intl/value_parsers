8a0309cc1f2a9e103ae1dd23f5dd88181f6311a9
Daniel Buttig <daniel.buttig@gmail.com>
2014-06-12 13:41:05 -0500

    Parses empty strings as nil instead of zero
    We don't want to parse null value as a zero on things like test scores.
    
    [WA-1763]
    Amos King @adkron <amos.l.king@gmail.com>
    Daniel Buttig @dbuttig <daniel.buttig@gmail.com>

--- spec/value_parsers/integer_parser_spec.rb
+++ spec/value_parsers/integer_parser_spec.rb
@@ -5,4 +5,18 @@ describe IntegerParser do
   describe 'parsing an integer' do
     it { IntegerParser.parse('36').must_equal 36 }
   end
+
+  describe "parsing a null" do
+    it { IntegerParser.parse(nil).must_equal nil }
+  end
+
+  describe "parsing a empty string" do
+    it { IntegerParser.parse("").must_equal nil }
+  end
+
+  describe "parsing a bad value" do
+    it do
+      proc { IntegerParser.parse("foo") }.must_raise(ArgumentError)
+    end
+  end
 end
